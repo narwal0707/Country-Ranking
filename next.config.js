ced212454f3ef7b08c3db366ca11229323201bb8
narwal0707 <abhisheknarwal408@gmail.com>
2022-12-02 11:41:26 +0530

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+// const withPWA = require('next-pwa')
+
+// module.exports = withPWA({
+//   future: {webpack5: true},
+//   pwa: {
+//     dest: 'public',
+//     fallbacks: {
+//       image: '/static/images/fallback.png',
+//     }
+//   }
+// })
+
+
+module.exports = {
+  future: {
+    webpack5: true
+  }
+}
